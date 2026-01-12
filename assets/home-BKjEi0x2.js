0ccfa8dee6f299413e4a611e6183a6d68b970e8e
OxoGhost <arlenghost@proton.me>
2026-01-12 15:42:56 +0100

    Updates

---
+++ assets/home-BKjEi0x2.js
@@ -0,0 +1,43 @@
+import{w as l,a as o,p as e,q as n}from"./chunk-FGUA77HG-DLcrTHtq.js";const d=l(function(){o.useEffect(()=>{document.title="Home - OxoGhost"},[]);const[t,r]=o.useState(!1);return e.jsxs("section",{className:"flex flex-col items-center text-center space-y-10 animate-fadeIn h-full justify-center",children:[e.jsx("img",{src:"/profile.png",alt:"Profile",className:"w-80 h-80 rounded-full object-cover shadow-2xl border border-white/30"}),e.jsxs("div",{className:"flex flex-col space-y-1",children:[e.jsx("h2",{className:"text-5xl font-bold",children:"Arlen Ghost"}),e.jsx("p",{className:"text-2xl text-gray-300",children:"CS Student & ML Enthusiast"})]}),e.jsxs("div",{className:"flex flex-col sm:flex-row gap-6 mt-6 w-full max-w-2xl",children:[e.jsx(n,{to:"/projects",className:`flex-1 
+                        bg-gradient-to-br from-purple-600/30 to-pink-600/30 
+                        backdrop-blur-xl 
+                        border border-white/20 
+                        py-3 px-6 rounded-xl text-xl font-medium text-nowrap
+                        hover:from-purple-500/50 hover:to-pink-500/50 
+                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"🚀 My Projects"}),e.jsx(n,{to:"/about",className:`flex-1 
+                        bg-gradient-to-br from-purple-600/30 to-pink-600/30 
+                        backdrop-blur-xl 
+                        border border-white/20 
+                        py-3 px-6 rounded-xl text-xl font-medium text-nowrap
+                        hover:from-purple-500/50 hover:to-pink-500/50 
+                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"👤 More About Me"}),e.jsxs("div",{className:"relative flex-1",children:[e.jsx("button",{onClick:()=>r(s=>!s),className:`
+                        flex-1 
+                        bg-gradient-to-br from-purple-600/30 to-pink-600/30 
+                        backdrop-blur-xl 
+                        border border-white/20 
+                        py-3 px-6 rounded-xl text-xl font-medium text-nowrap
+                        hover:from-purple-500/50 hover:to-pink-500/50 
+                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]
+                        `,children:"📄 Download CV"}),t&&e.jsxs("div",{className:"fixed inset-0 z-50 flex items-center justify-center",children:[e.jsx("div",{className:"absolute inset-0 backdrop-blur-sm",onClick:()=>r(!1)}),e.jsxs("div",{className:`
+                            relative z-10 w-80
+                            bg-black/70 backdrop-blur-xl
+                            border border-white/20
+                            rounded-2xl
+                            p-6
+                            shadow-2xl
+                            text-center
+                        `,children:[e.jsx("h3",{className:"text-xl font-semibold mb-2",children:"Choose CV language"}),e.jsx("p",{className:"text-sm text-gray-300 mb-6",children:"Select the version you want to download"}),e.jsxs("div",{className:"flex gap-4",children:[e.jsx("a",{href:"/ArlenGhost_CV_EN.pdf",download:!0,className:`
+                                flex-1
+                                py-3 rounded-xl
+                                bg-gradient-to-br from-purple-600/30 to-pink-600/30
+                                border border-white/20
+                                hover:from-purple-500/40 hover:to-pink-500/40
+                                transition
+                            `,children:"🇬🇧 English"}),e.jsx("a",{href:"/ArlenGhost_CV_FR.pdf",download:!0,className:`
+                                flex-1
+                                py-3 rounded-xl
+                                bg-gradient-to-br from-purple-600/30 to-pink-600/30
+                                border border-white/20
+                                hover:from-purple-500/40 hover:to-pink-500/40
+                                transition
+                            `,children:"🇫🇷 Français"})]}),e.jsx("button",{onClick:()=>r(!1),className:"mt-6 text-sm text-gray-400 hover:text-white transition",children:"Cancel"})]})]})]})]})]})});export{d as default};
