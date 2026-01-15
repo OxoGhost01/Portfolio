3b430ac2c8871d00fdb42ca8798be308604b926e
OxoGhost <arlenghost@proton.me>
2026-01-15 16:06:43 +0100

    Added this portfolio code source

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -136,6 +136,33 @@ function ProjectsLoader() {
 
                 {/* Project 3 */}
                 <a
+                    href="https://github.com/OxoGhost01/Portfolio"
+                    target="_blank"
+                    className="group bg-white/10 backdrop-blur rounded-xl border border-white/20 hover:bg-white/20 transition cursor-pointer flex overflow-hidden"
+                >
+                    <div className="relative w-32 h-32 sm:w-36 sm:h-36 border-r border-white/10 flex-shrink-0 overflow-hidden rounded-xl">
+                        <img
+                            src="/projects/porfolio.png"
+                            alt="Portfolio"
+                            className="absolute inset-0 w-full h-full object-cover"
+                        />
+                    </div>
+
+                    <div className="p-4 flex flex-col justify-center">
+                        <h3 className="font-semibold text-xl mb-1">Portfolio</h3>
+                        <p className="text-gray-300 text-sm mb-3">
+                            My portfolio website built with React and TypeScript.
+                        </p>
+                        <div className="flex flex-wrap gap-2">
+                            <span className="px-3 py-1 rounded-full text-sm bg-gradient-to-br from-purple-600/30 to-pink-600/30 border border-white/10 shadow-[0_0_10px_rgba(255,0,255,0.25)]">
+                                TypeScript
+                            </span>
+                        </div>
+                    </div>
+                </a>
+
+                {/* Project 3 */}
+                <a
                     href="https://github.com/OxoGhost01/Clone_YTS_But_Strange"
                     target="_blank"
                     className="group bg-white/10 backdrop-blur rounded-xl border border-white/20 hover:bg-white/20 transition cursor-pointer flex overflow-hidden"
