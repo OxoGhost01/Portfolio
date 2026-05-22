7f6d17e48d0798ce705c454442824872b9ae5ff3
OxoGhost <arlenghost@proton.me>
2026-05-22 12:08:39 +0200

    Updates

--- assets/home-DpJJcBBS.js
+++ assets/home-DpJJcBBS.js
@@ -1,16 +1,16 @@
-import{w as l,a as n,p as e,q as o}from"./chunk-FGUA77HG-DLcrTHtq.js";const d=l(function(){n.useEffect(()=>{document.title="Home - OxoGhost"},[]);const[t,r]=n.useState(!1);return e.jsxs("section",{className:"flex flex-col items-center text-center space-y-10 animate-fadeIn h-full justify-center",children:[e.jsx("img",{src:"/profile.png",alt:"Profile",className:"w-80 h-80 rounded-full object-cover shadow-2xl border border-white/30"}),e.jsxs("div",{className:"flex flex-col space-y-1",children:[e.jsx("h2",{className:"text-5xl font-bold",children:"Arlen Ghost"}),e.jsx("p",{className:"text-2xl text-gray-300",children:"CS Student & ML Enthusiast"})]}),e.jsxs("div",{className:"flex flex-col sm:flex-row gap-6 mt-6 w-full max-w-2xl",children:[e.jsx(o,{to:"/projects",className:`flex-1 
+import{w as s,a as o,p as e,q as n}from"./chunk-FGUA77HG-DLcrTHtq.js";const i=s(function(){o.useEffect(()=>{document.title="Home - OxoGhost"},[]);const[t,r]=o.useState(!1);return e.jsxs("section",{className:"flex flex-col items-center text-center space-y-10 animate-fadeIn h-full justify-center",children:[e.jsx("img",{src:"/profile.png",alt:"Profile",className:"w-80 h-80 rounded-full object-cover shadow-2xl border border-white/30"}),e.jsxs("div",{className:"flex flex-col space-y-1",children:[e.jsx("h2",{className:"text-5xl font-bold",children:"Arlen Ghost"}),e.jsx("p",{className:"text-2xl text-gray-300",children:"CS Student & Low-Level Enjoyer"})]}),e.jsxs("div",{className:"flex flex-col sm:flex-row gap-6 mt-6 w-full max-w-2xl",children:[e.jsx(n,{to:"/projects",className:`flex-1 
                         bg-gradient-to-br from-purple-600/30 to-pink-600/30 
                         backdrop-blur-xl 
                         border border-white/20 
                         py-3 px-6 rounded-xl text-xl font-medium text-nowrap
                         hover:from-purple-500/50 hover:to-pink-500/50 
-                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"🚀 My Projects"}),e.jsx(o,{to:"/about",className:`flex-1 
+                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"🚀 My Projects"}),e.jsx(n,{to:"/about",className:`flex-1 
                         bg-gradient-to-br from-purple-600/30 to-pink-600/30 
                         backdrop-blur-xl 
                         border border-white/20 
                         py-3 px-6 rounded-xl text-xl font-medium text-nowrap
                         hover:from-purple-500/50 hover:to-pink-500/50 
-                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"👤 More About Me"}),e.jsxs("div",{className:"relative flex-1",children:[e.jsx("button",{onClick:()=>r(s=>!s),className:`
+                        transition shadow-[0_0_20px_rgba(255,0,255,0.2)] hover:shadow-[0_0_25px_rgba(255,0,255,0.4)]`,children:"👤 More About Me"}),e.jsxs("div",{className:"relative flex-1",children:[e.jsx("button",{onClick:()=>r(l=>!l),className:`
                         flex-1 
                         bg-gradient-to-br from-purple-600/30 to-pink-600/30 
                         backdrop-blur-xl 
@@ -40,4 +40,4 @@ import{w as l,a as n,p as e,q as o}from"./chunk-FGUA77HG-DLcrTHtq.js";const d=l(
                                 border border-white/20
                                 hover:from-purple-500/40 hover:to-pink-500/40
                                 transition
-                            `,children:"🇫🇷 Français"})]}),e.jsx("button",{onClick:()=>r(!1),className:"mt-6 text-sm text-gray-400 hover:text-white transition",children:"Cancel"})]})]})]})]})]})});export{d as default};
+                            `,children:"🇫🇷 Français"})]}),e.jsx("button",{onClick:()=>r(!1),className:"mt-6 text-sm text-gray-400 hover:text-white transition",children:"Cancel"})]})]})]})]})]})});export{i as default};
